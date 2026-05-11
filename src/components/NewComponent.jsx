ca8cdfa77570370cee8d1bee79f9c1e752c44bb9
prashantttzz <contact.prashant31@gmail.com>
2026-05-11 17:16:38 +0530

    chapter 9: conditionally rendering

--- src/components/NewComponent.jsx
+++ src/components/NewComponent.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const NewComponent = ({name,id,...user}) => {
+
   return (
-    <div>{name}
-    {/* <h1>{...user}</h1> */}
+    <div>
+      <h2>{name}</h2>
+      {name==="prashant"?"welcome prashant":"who are you?"}
+      {/* ternary operator */}
+      <p>{name && `heelo ${name}`}</p>
+      {/* conditionally rendering  */}
+      <p>{name ??"prashant"}</p>
+      {/* nullish provide a feedback if null or undefined */}
+
     </div>
     
   )
